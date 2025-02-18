4f02ee270930b54d45442f4d7d7b66c45f850781
Josef Válek <valek.josef@seznam.cz>
2025-02-18 22:02:21 +0100

    Add deploy job

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jaroslav-pavlicek.cz',
+
   vite: {
     plugins: [tailwindcss()]
   },
